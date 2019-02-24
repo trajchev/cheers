5564ebf58f85b1d41464bb25ded5c954a343f48d
Ivan <ivan.trajchev@symphony-solutions.eu>
2019-02-24 21:21:07 +0100

    About section

--- front-page.php
+++ front-page.php
@@ -56,6 +56,22 @@ get_header();
         </div>
     </section>
 
+    <section id="about-section">
+        <div class="container">
+            <div class="grey-gradient up-rounded px-4">
+                <article class="mx-auto article-block py-4">
+                    <div class="article-header">
+                        <h2 class="h4 text-center py-0 my-0"><?php _e('About us', 'cheers'); ?></h2>
+                        <h3 class="h6 text-center py-0"><?php _e('Get to know us', 'cheers'); ?></h2>
+                    </div>
+                    <div class="article-content">
+                        <?php the_field('about_section'); ?>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </section>
+
 </div><!-- #index-wrapper -->
 
 <?php get_footer(); ?>
