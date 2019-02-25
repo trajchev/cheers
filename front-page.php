832ec05b48bb0a078ed93fe38e2f57687bd10a90
Ivan <ivan.trajchev@symphony-solutions.eu>
2019-02-25 04:43:56 +0100

    Workflow section

--- front-page.php
+++ front-page.php
@@ -72,6 +72,87 @@ get_header();
         </div>
     </section>
 
+    <section id="workflow-section">
+        <div class="container">
+            <div class="bg-banana top-border-14 py-2">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card text-center container mb-4">
+                            <header class="card-header">
+                                <h3>Research</h3>
+                            </header>
+                            <p>
+                                Donec pellentesque, justo eget imperdiet viverra, lectus mi ornare arcu, non laoreet lacus lacus eleifend orci. Nam tincidunt vestibulum lacus nec pharetra. Nunc commodo aliquam lectus quis bibendum.
+                            </p>
+                            <footer class="card-footer">
+                                <a href="#" class="btn btn-dark">learn more</a>
+                            </footer>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card text-center container mb-4">
+                            <header class="card-header">
+                                <h3>Work</h3>
+                            </header>
+                            <p>
+                                Donec pellentesque, justo eget imperdiet viverra, lectus mi ornare arcu, non laoreet lacus lacus eleifend orci. Nam tincidunt vestibulum lacus nec pharetra. Nunc commodo aliquam lectus quis bibendum.
+                            </p>
+                            <footer class="card-footer">
+                                <a href="#" class="btn btn-dark">learn more</a>
+                            </footer>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card text-center container mb-4">
+                            <header class="card-header">
+                                <h3>Deliver</h3>
+                            </header>
+                            <p>
+                                Donec pellentesque, justo eget imperdiet viverra, lectus mi ornare arcu, non laoreet lacus lacus eleifend orci. Nam tincidunt vestibulum lacus nec pharetra. Nunc commodo aliquam lectus quis bibendum.
+                            </p>
+                            <footer class="card-footer">
+                                <a href="#" class="btn btn-dark">learn more</a>
+                            </footer>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- <div class="row"> -->
+                    <div class="container">
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs slider-tabs" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link grey-gradient up-rounded active" data-toggle="tab" href="#home">Latest Press</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link grey-gradient up-rounded" data-toggle="tab" href="#menu1">Testimonials</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link grey-gradient up-rounded" data-toggle="tab" href="#menu2">Blog Posts</a>
+                            </li>
+                        </ul>
+
+                        <!-- Tab panes -->
+                        <div class="tab-content grey-gradient up-rounded">
+                            <div id="home" class="container tab-pane active"><br>
+                                <h3>Latest Posts</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <div id="menu1" class="container tab-pane fade"><br>
+                                <h3>Testimonials</h3>
+                                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            </div>
+                            <div id="menu2" class="container tab-pane fade"><br>
+                                <h3>Blog Posts</h3>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                            </div>
+                        </div>
+                    <!-- </div> -->
+                </div>
+            </div>
+        </div>
+    </section>
+
 </div><!-- #index-wrapper -->
 
 <?php get_footer(); ?>
