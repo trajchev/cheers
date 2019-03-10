9def25843286190b1a57e3600e16f2037fdb92f9
Ivan <ivan.trajchev@symphony-solutions.eu>
2019-03-10 23:47:45 +0100

    About refactored

--- partials/_about.php
+++ partials/_about.php
@@ -1,7 +1,7 @@
 <!-- THE ABOUT SECTION -->
 <section id="about-section">
     <div class="container">
-        <div class="grey-gradient up-rounded px-4">
+        <div class="grey-gradient up-rounded px-4 pb-2">
             <article class="mx-auto article-block py-4">
                 <div class="article-header">
                     <h2 class="h4 text-center py-0 my-0"><?php _e('About us', 'cheers'); ?></h2>
