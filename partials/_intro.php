573cbcff1bc2907b39c1b2ae203ddd5ecce3cc4e
Ivan <ivan.trajchev@symphony-solutions.eu>
2019-03-15 09:22:07 +0100

    Added carousel

--- partials/_intro.php
+++ partials/_intro.php
@@ -3,6 +3,7 @@
     <div class="container">
 
         <div class="carousel-wrapper py-4 grey-gradient up-rounded dwn-rounded mb-2">
+
             <?php
 
                 $carousel = new WP_Query(
@@ -17,78 +18,62 @@
 
                 );
 
+                $i = 0;
+
             ?>
 
+            <div id="cheers-carousel" class="carousel slide" data-ride="carousel">
 
-            <?php if ( $carousel->have_posts() ) : ?>
-
-            <div id="demo" class="carousel slide" data-ride="carousel">
-
-                <ul class="carousel-indicators">
-                    <li data-target="#demo" data-slide-to="0" class="active"></li>
-                    <li data-target="#demo" data-slide-to="1"></li>
-                    <li data-target="#demo" data-slide-to="2"></li>
-                </ul>
+                <?php  if ( $carousel->have_posts() ) : ?>
 
                 <div class="carousel-inner">
 
-
                     <?php while ( $carousel->have_posts() ) : ?> 
 
-                    <?php $i = 0; $carousel->the_post(); ?>
+                    <?php $carousel->the_post(); ?>
 
                     <?php
 
-                        
-                            if ($i == 0) : ?>
-                                <div class="carousel-item active">
-                                    <img src="https://www.w3schools.com/bootstrap4/la.jpg" alt="Los Angeles" width="1100" height="500">
-                                    <div class="carousel-caption">
-                                        <h3>Los Angeles</h3>
-                                        <p>We had such a great time in LA!</p>
-                                    </div>   
-                                </div>
-                            <?php else: ?>
-                                <div class="carousel-item">
-                                    <img src="https://www.w3schools.com/bootstrap4/la.jpg" alt="Los Angeles" width="1100" height="500">
-                                    <div class="carousel-caption">
-                                        <h3>Los Angeles</h3>
-                                        <p>We had such a great time in LA!</p>
-                                    </div>   
-                                </div>
-                            <?php endif;  $i++; ?>
+                        if ($i == 0) : ?>
+
+                            <div class="carousel-item active">
+
+                        <?php else: ?>
+
+                            <div class="carousel-item">
+
+                        <?php endif;  $i++; ?>
+
+                                <?php the_post_thumbnail(); ?>
+
+                                <div class="carousel-caption">
+
+                                    <a href="<?php the_permalink(); ?>">
+
+                                        <h3><?php the_title(); ?></h3>
+
+                                    </a>
+
+                                </div>   
+
+                            </div>
 
                     <?php endwhile; ?>
-
-                    <!-- <div class="carousel-item">
-                        <img src="https://www.w3schools.com/bootstrap4/chicago.jpg" alt="Chicago" width="1100" height="500">
-                        <div class="carousel-caption">
-                            <h3>Chicago</h3>
-                            <p>Thank you, Chicago!</p>
-                        </div>   
-                    </div>
-
-                    <div class="carousel-item">
-                        <img src="https://www.w3schools.com/bootstrap4/ny.jpg" alt="New York" width="1100" height="500">
-                        <div class="carousel-caption">
-                            <h3>New York</h3>
-                            <p>We love the Big Apple!</p>
-                        </div>   
-                    </div> -->
+                    
                 </div>
 
-                <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <a class="carousel-control-prev" href="#cheers-carousel" data-slide="prev">
                     <i class="fa fa-glass fa-rotate-90" aria-hidden="true"></i>
                 </a>
 
-                <a class="carousel-control-next" href="#demo" data-slide="next">
+                <a class="carousel-control-next" href="#cheers-carousel" data-slide="next">
                     <i class="fa fa-glass fa-rotate-270" aria-hidden="true"></i>
                 </a>
 
+                <?php endif; ?>
+
             </div>
 
-            <?php endif; ?>
-        
         </div>
     
     </div>
